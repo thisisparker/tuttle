79e7d06b3e1bdb2315f16599ff0b48f81f4d1bde
Parker Higgins <parker@parkerhiggins.net>
2018-10-10 15:18:55 -0400

    added config page stub

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import configpage
 import readlog
 import sendmsg
 
@@ -8,7 +9,7 @@ from flask import Flask, request, send_from_directory
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
+def serve_home():
     if request.method == 'POST':
         recipient = request.form.get('recipient')
         message = request.form.get('message')
@@ -16,6 +17,10 @@ def hello_world():
         sendmsg.send(recipient, message)
 
     return readlog.main() 
+
+@app.route('/config', methods=['GET', 'POST'])
+def serve_config():
+    return configpage.main()
 
 @app.route('/style.css')
 def serve_style():
