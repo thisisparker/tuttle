b6f5509cddad040dca380d366361ae6666aa180e
Parker Higgins <parker@parkerhiggins.net>
2018-10-10 23:08:19 -0400

    added not-yet-functional config options

--- configpage.py
+++ configpage.py
@@ -8,6 +8,10 @@ from dominate.tags import *
 
 from settings import NUMBER, database
 
+userlist = [{'user':'useruseruser', 'number': '+12125551212'},
+            {'user':'testtesttest', 'number': '+12028675309'},
+           ]
+
 def main():
     h = dominate.document()
     h.title = 'configuration'
@@ -18,5 +22,29 @@ def main():
     body = h.add(div(id='body'))
 
     body.add(h1("configuration"))
+
+    body.add(h2('notifications'))
+
+    notification_options = body.add(table(id='notifications-table'))
+
+    for user in userlist:
+        notification_options.add(tr(td(user['user']), td(user['number']),
+            td(a('❌', href="#"))))
+
+    config_options = body.add(form(method='post', id='config-options'))
+
+    with config_options.add(ul()):
+        li(label(h2("autoresponder"), fr='autoresponder', __pretty=False),
+           textarea(id="autoresponder", name="autoresponder",
+                    rows=6))
+        li(label(h2("authentication options"), fr='auth', __pretty=False),
+            fieldset(
+                input_(type='radio', id='rotate-onions', name='auth'),
+                label("Rotate Onion URLs", fr='rotate-onions'), br(),
+                input_(type='radio', id='hidservauth', name='auth'),
+                label("Use HidServAuth authentication", fr='hidservauth')))
+
+    config_options.add(input_(type='submit', value='Save settings', 
+        id='save-button'))
 
     return h.render()
