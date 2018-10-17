b9e0ebe479a07960836493c87723a56bc7eac2ec
Parker Higgins <parker@parkerhiggins.net>
2018-10-17 11:45:12 -0400

    added attachment deletion to delete action

--- deletemsg.py
+++ deletemsg.py
@@ -1,16 +1,26 @@
 #!/bin/env python3
 
+import os
 import sqlite3
 
 from settings import database
+from logincoming import sitepath
 
 def single_msg(msgid):
     conn = sqlite3.connect(database)
 
+    attachment = conn.execute(
+            'select attachments from messages where rowid = ?',
+            [msgid]).fetchone()
+    attachment = attachment[0]
+    if attachment:
+        attachments_dir = os.path.join(sitepath, 'attachments')
+        attachment_path = os.path.join(attachments_dir, attachment)
+        os.remove(attachment_path)
+    
     conn.execute('delete from messages where rowid = ?', [msgid])
 
     conn.commit()
-
     conn.close()
 
 def msg_thread(number):
@@ -19,7 +29,6 @@ def msg_thread(number):
     rowids = conn.execute(
             'select rowid from messages where recipient = ? or source = ?',
             [number, number]).fetchall()
-
     rowids = [rowid[0] for rowid in rowids]
 
     conn.close()
